81fea1467573f06994b5bdab3517dbf4c3d45d51
Grace9994 <luyiwen0802@outlook.com>
2024-06-14 16:11:52 +0800

    advice

---
+++ files.py
@@ -0,0 +1,5 @@
+import glob
+
+files=glob.glob('C:\Users\a\Desktop\demo\lycen0904.github.io\cmos_long\*\*.wav')
+for i in
+"<td style=" text-align: center"><audio controls="controls" ><source src="+i+
